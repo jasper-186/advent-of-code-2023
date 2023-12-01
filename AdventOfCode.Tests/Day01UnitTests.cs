9d6832a0a36a5b75dbe3c6f1ad64152800e899ac
Joe Echtenkamp <github@echtenkamp.com>
2023-12-01 13:39:02 -0600

    Still trying to figure out unitesting logging

--- AdventOfCode.Tests/Day01UnitTests.cs
+++ AdventOfCode.Tests/Day01UnitTests.cs
@@ -2,12 +2,15 @@ namespace AdventOfCode.Tests;
 
 using AdventOfCode.Day01;
 
+
 public class Day01UnitTests:BaseUnitTests
 {
     [Fact]
     public void Puzzle01UnitTest()
     {        
-        var result = Puzzle01.solve(baseFilePath +"SampleInputs/Day01.Puzzle01.Input.Sample.txt");
+        var logger = Mock.Of<ILogger<Puzzle01>>();
+        var puzzle=new Puzzle01(logger);
+        var result = puzzle.solve(baseFilePath +"SampleInputs/Day01.Puzzle01.Input.Sample.txt");
         Assert.Equal(142, result);
     }
 }
