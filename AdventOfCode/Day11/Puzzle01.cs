6cdf8620263fd4b6a780ea80866e32f32b23892c
Joe Echtenkamp <github@echtenkamp.com>
2024-07-02 15:08:46 -0500

    Adding changed files before removing repo from makehare

---
+++ AdventOfCode/Day11/Puzzle01.cs
@@ -0,0 +1,21 @@
+namespace AdventOfCode.Day11;
+
+using AdventOfCode.Common;
+using Microsoft.Extensions.Logging;
+
+public class Puzzle01 : PuzzleInterface
+{
+    private readonly ILogger _logger;
+
+    public Puzzle01(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<Puzzle01>();
+    }
+
+    public long solve(string filepath)
+    {
+        var lines = FileUtils.ReadFileLines(filepath);
+       var pf = new PipeField(lines);
+       return pf.Traverse();
+    }
+}
